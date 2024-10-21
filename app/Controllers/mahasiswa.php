366d5adc52276a5c6e45c9fc0630f42eac305cec
UmarFauzy <125781380+UmarFauzy@users.noreply.github.com>
2024-10-22 06:05:59 +0700

    Membuat Aplikasi MVC dengan PHP #11 Delete Data - Beres

--- app/Controllers/mahasiswa.php
+++ app/Controllers/mahasiswa.php
@@ -32,6 +32,19 @@ class mahasiswa extends Controller {
         }
     }
 
+    public function hapus($id) 
+    {
+        if ( $this->model('mahasiswa_model')->hapusDataMahasiswa($id) > 0 ) {
+            Flasher::setFlash('berhasil', 'dihapus', 'success');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('gagal', 'dihapus', 'danger');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
+
 
 
 
